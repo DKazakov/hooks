eafeab1daca748560c43d1f5feaea2148453feaa
Казаков Дмитрий Владимирович <d.kazakov@prototypes.ru>
2016-07-18 19:25:39 +0300

    second

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+CURRENT=`pwd`
 if [ -d ~/.git-workaround ];then
     cd ~/.git-workaround
     git pull
-    bash ./install.sh
+    cd $CURRENT
+    bash ~/.git-workaround/install.sh
 else
     git clone gitlab@gitlab.prototypes.ru:d.kazakov/git-workaround.git ~/.git-workaround
     bash ~/.git-workaround/install.sh
