8598f70f6efef45159ef27e31323b57142dbc8a2
Казаков Дмитрий Владимирович <d.kazakov@prototypes.ru>
2016-07-18 19:22:39 +0300

    second

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "YEP"
+if [ -d ~/.git-workaround ];then
+    cd ~/.git-workaround
+    git pull
+    bash ./install.sh
+else
+    git clone gitlab@gitlab.prototypes.ru:d.kazakov/git-workaround.git ~/.git-workaround
+    bash ~/.git-workaround/install.sh
+fi
